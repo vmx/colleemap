6b4fd56faf272708081e30bd15d3273a8126ed5b
Volker Mische <volker.mische@gmail.com>
2023-11-10 19:35:46 +0100

    chore: remove hard-coded selected items
    Those were only be there for debugging purpose.

--- src/stores.js
+++ src/stores.js
@@ -4,14 +4,6 @@ export const name = writable('')
 export const messages = writable([])
 // Store all items that were selected by anyone. They key is the ID of the
 // item, the value is an array of PeerIDs that selected that item.
-export const selectedStore = writable({
-  3: ['some-peer-id'],
-  20: ['some-peer-id2'],
-  6: ['some-peer-id6'],
-  16: ['some-peer-id16'],
-  18: ['some-peer-id18'],
-  23: ['some-peer-id23'],
-  25: ['some-peer-id25']
-})
+export const selectedStore = writable({})
 // Whenever somone has a Bingo we save that. It's just an array of strings.
 export const winnersStore = writable([])
