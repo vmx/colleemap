e435567b5d616bd6dc535c01f7565a9ede31baf1
Volker Mische <volker.mische@gmail.com>
2023-11-12 19:52:30 +0100

    fix: things should work

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
   import { createLibp2p } from 'libp2p'
   import { webRTCNoNat } from '@libp2p/webrtc'
-  import { FaultTolerance } from '@libp2p/interface-transport'
+  import { FaultTolerance } from '@libp2p/interface/transport'
   //import * as IPFS from 'ipfs-core'
   import { gossipsub } from '@chainsafe/libp2p-gossipsub'
   import { plaintext } from 'libp2p/insecure'
